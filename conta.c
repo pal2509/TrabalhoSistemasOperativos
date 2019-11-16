1bee3d2a4c56e489beed4eadd06c0ff39ba39a46
pal2509 <paulocapa55@gmail.com>
2019-11-16 11:21:58 +0000

    Mostra e Conta

--- conta.c
+++ conta.c
@@ -3,6 +3,8 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <stdio.h>
+#include <stdlib.h>
+
 
 int main(int argc, char* argv[])
 {
@@ -17,7 +19,13 @@ int main(int argc, char* argv[])
             int size = file.st_size; //Guardar o tamanho do ficheiro
             char str[size]; //Criar um buffer do tamanho do ficheiro
             read(fd,str,size); //Ler o ficherio para a str
-            write(1,str,size); //Escrever no ecrã a Str
+            
+            int lines = 0; //Contador do numero de linhas
+            for(int k = 0 ; k < size ; k++) //Percorrer a string
+            {
+                if(str[k] == '\n') lines++; //Contar os \n na string
+            }
+            printf("Número de linhas de %s => %d\n", argv[i], lines); //Escrever o numero de linhas
         }
         else write(2,"Erro ficherio não existe!!!\n", 30); //Mensagem de erro
         close(fd); //Fechar o fiheiro
