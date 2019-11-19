dddfe572b296540e8f0d448da4f1f66d6f9d468d
CAPPA2ZZ <paulocapa55@gmail.com>
2019-11-19 16:27:17 +0000

    Informa

--- informa.c
+++ informa.c
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 
 int main(int argc, char* argv[])
@@ -17,12 +18,21 @@ int main(int argc, char* argv[])
         {
             stat(argv[i], &file); //Estado do ficherio
             printf("Nome: %s\n", argv[i]);
-            printf("Tipo: %s\n",file.st_mode);
-            printf("Tamanho: %s\n",file.st_size);
-            printf("I-node: %s\n",file.st_ino);
-            printf("Dono: %s\n",file.st_uid);
-            printf("Criação: %s\n");
-            printf("Última modificação: %s\n",file.st_mtime);
+            printf("Tipo de ficheiro: ");
+            switch (file.st_mode & __S_IFMT) {
+            case __S_IFBLK:  printf("Orientado ao bloco\n");      break;
+            case __S_IFCHR:  printf("Orientado ao caracter\n");   break;
+            case __S_IFIFO:  printf("FIFO/pipe\n");               break;
+            case __S_IFLNK:  printf("symlink\n");                 break;
+            case __S_IFREG:  printf("Normal\n");                  break;
+            case __S_IFSOCK: printf("Socket\n");                  break;
+            default:       printf("Desconhecido?\n");             break;
+           }
+            printf("Tamanho: %lld bytes\n",(long long)file.st_size);
+            printf("I-node: %ld\n",(long)file.st_ino);
+            printf("UID: %ld \n",(long)file.st_uid);
+            printf("Criação: \n");
+            printf("Última modificação: %s\n",ctime(&file.st_mtime));
 
         }
         else write(2,"Erro ficherio não existe!!!\n", 30); //Mensagem de erro
